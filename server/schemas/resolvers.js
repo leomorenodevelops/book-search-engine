725a9786ce24a76a6c7e37470817a8cb5c061dc5
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-19 16:52:42 -0700

    Created query resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../routes');
+
+const resolvers = {
+    Query: {
+        // Get user by username
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('books')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in')
+        },
+    },
+    
+}
