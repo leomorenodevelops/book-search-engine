2db1beb60e6b7022f1ca84fed4731515dd940dc9
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-19 15:11:10 -0700

    Created user type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,4 +12,11 @@ const typeDefs = gql`
     link: String
     title: String
   }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 `
