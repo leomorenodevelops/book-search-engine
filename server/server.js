9df25d4e7dc8ea294549da6bdaa6bd6a020a8ae2
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-19 13:39:29 -0700

    Get all

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const server = new ApolloServer({
 // Integrate our Apollo server with Express application as middleware
 server.applyMiddleware({app});
 
+// Middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -35,6 +36,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// Get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () =>  {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
