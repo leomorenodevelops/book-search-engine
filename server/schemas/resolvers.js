ac63c8dca239c7c3600d4c99b733103225649df9
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-19 17:09:24 -0700

    Created login for mutation resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,13 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../routes');
+const { sign } = require('jsonwebtoken');
 
 const resolvers = {
     Query: {
         // Get user by username
         me: async (parent, args, context) => {
-            if(context.user) {
+            if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id})
                 .select('-__v -password')
                 return userData;
@@ -20,6 +21,17 @@ const resolvers = {
             const token = signToken(user);
             return { token, user};
         },
-
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Invalid credentials')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Invalid credentials')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
     }
 }
