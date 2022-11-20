046b799c0d9b3f63260d102aecfebd3a89375c31
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-11-19 17:18:09 -0700

    Created save book for mutation resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,5 +33,17 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book } },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You must be logged in!')
+        },
+        
     }
 }
